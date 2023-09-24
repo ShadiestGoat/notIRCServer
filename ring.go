eba09d6ed84f9c0c797752724cd1a959bd767ff7
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-09-24 04:15:00 +0100

    Dont flip the message order

--- ring.go
+++ ring.go
@@ -29,15 +29,10 @@ func (r *Ring[T]) Insert(v T) {
 
 func (r *Ring[T]) Items() []T {
 	r.RLock()
+	defer r.RUnlock()
 	
 	s := make([]T, len(r.items))
 	copy(s, r.items)
-	
-	r.RUnlock()
-		
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-        s[i], s[j] = s[j], s[i]
-    }
-	
+
 	return s
 }
